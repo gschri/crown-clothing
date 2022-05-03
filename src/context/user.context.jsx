04a22822fc4007f12cb466b34fa06eeaef78c4a4
gschri <gabrielschri@gmail.com>
2022-05-03 09:34:23 -0300

    started using reducers for updating user context state

--- src/context/user.context.jsx
+++ src/context/user.context.jsx
@@ -1,14 +1,40 @@
-import {createContext,useState,useEffect} from 'react'
+import {createContext,useEffect,useReducer} from 'react'
 import { onAuthStateChangedListener,createUserDocumentFromAuth} from '../utils/firebase/firebase.utils';
 
-// as the actual value you want to access
 export let UserContext = createContext({
     currentUser: null,
     setCurrentUser: () => null,
 })
 
+export let USER_ACTION_TYPES = {
+    SET_CURRENT_USER: 'SET_CURRENT_USER'
+}
+
+export let userReducer = (state,action) => {
+    var {type,payload} = action;
+
+    switch(type) {
+        case USER_ACTION_TYPES.SET_CURRENT_USER:
+            return {
+                ...state,
+                currentUser: payload
+            }
+        default:
+            throw new Error(`Unhandled type ${type} in userReducer`)
+    }
+}
+
+export let INITIAL_STATE = {
+    currentUser: null
+}
+
 export let UserProvider = ({children}) => {
-    var [currentUser,setCurrentUser ] = useState(null); 
+    var [{currentUser}, dispatch] = useReducer(userReducer,INITIAL_STATE)
+
+    var setCurrentUser = (user) => {
+        dispatch({type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user })
+    }
+
     var value = {currentUser,setCurrentUser} 
 
     useEffect(() => {
