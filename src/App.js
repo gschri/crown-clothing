b0120066f85033414badf8c0df214ea25d94e863
gschri <gabrielschri@gmail.com>
2022-05-06 14:03:26 -0300

    added dynamic imports to App.js in order to cut down on bundle.js size and improve performance

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from "./routes/home/home.component";
-import Navigation from './routes/navigation/navigation.component'
-import Authentication from './routes/authentication/authentication.component';
-import Shop from './routes/shop/shop.component'
-import Checkout from './routes/checkout/checkout.component'
-import { useEffect } from 'react'
+import { useEffect, lazy, Suspense } from 'react'
 import { useDispatch } from 'react-redux'
+import { Routes, Route } from 'react-router-dom';
+import Spinner from './components/spinner/spinner.component'
 import { checkUserSession } from './store/user/user.action'
+
+let Home = lazy(() => import("./routes/home/home.component"))
+let Authentication = lazy(() => import('./routes/authentication/authentication.component'))
+let Navigation = lazy(() => import('./routes/navigation/navigation.component'))
+let Shop = lazy(() => import('./routes/shop/shop.component'))
+let Checkout = lazy(() => import('./routes/checkout/checkout.component'))
 
 let App = () => {
   var dispatch = useDispatch();
@@ -15,14 +17,16 @@ let App = () => {
   }, [])
 
   return (
-    <Routes>
-      <Route path='/' element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path='shop/*' element={<Shop />} />
-        <Route path='auth' element={<Authentication />} />
-        <Route path='checkout' element={<Checkout />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<Spinner />}>
+      <Routes>
+        <Route path='/' element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path='shop/*' element={<Shop />} />
+          <Route path='auth' element={<Authentication />} />
+          <Route path='checkout' element={<Checkout />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 
